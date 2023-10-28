1a0ed4c4da6985e3ec59184d5b88bb9e58572689
bialger <artur.bigulov@yandex.ru>
2023-10-28 04:19:30 +0300

    Added sand pile implementation

--- lib/sand_pile/sand_pile.hpp
+++ lib/sand_pile/sand_pile.hpp
@@ -1,6 +1,18 @@
 #ifndef SAND_PILE_HPP
 #define SAND_PILE_HPP
 
-class SandPile {};
+#include "lib/field/coordinates_field.hpp"
+
+class SandPile {
+private:
+  CoordinatesField pile_;
+  uint64_t current_iter_;
+
+  bool Collapse();
+public:
+  SandPile();
+  void BeginCollapsing(char* filename, char* dirname, uint64_t max_iterations,
+                       uint64_t frequency);
+};
 
 #endif // SAND_PILE_HPP
