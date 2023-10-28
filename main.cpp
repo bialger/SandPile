414d5bceb32828eceeb70ad418244998bd395d20
bialger <artur.bigulov@yandex.ru>
2023-10-28 04:20:43 +0300

    Implemented main logics

--- main.cpp
+++ main.cpp
@@ -1,9 +1,20 @@
+#include "lib/basic/basic_functions.hpp"
 #include "lib/parser/arguments_parser.hpp"
-
-#include <iostream>
+#include "lib/sand_pile/sand_pile.hpp"
 
 int main(int32_t argc, char** argv) {
   ArgumentsParser arguments_parser = ArgumentsParser();
   int8_t exit_code = arguments_parser.ParseArguments(argv, argc);
+
+  if (exit_code == 0 && !arguments_parser.GetProvideHelp()) {
+    SandPile sand_pile = SandPile();
+    sand_pile.BeginCollapsing(arguments_parser.GetInputFile(),
+                              arguments_parser.GetOutputDirectory(),
+                              arguments_parser.GetMaxIterations(),
+                              arguments_parser.GetFrequency());
+  } else {
+    PrintHelp();
+  }
+
   return exit_code;
 }
