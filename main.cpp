f1cc13d3d2170db079709a683d5406a210eb19fb
bialger <artur.bigulov@yandex.ru>
2023-11-04 15:57:35 +0300

    Review fix: removed redundant variables from main

--- main.cpp
+++ main.cpp
@@ -3,30 +3,26 @@
 #include "lib/sand_pile/sand_pile.hpp"
 
 int main(int32_t argc, char** argv) {
-  bool (* is_valid_filename_pointer)(char*) = &IsValidFilename;
-  bool (* is_regular_file_pointer)(char*) = &IsRegularFile;
-  bool (* is_directory_pointer)(char*) = &IsDirectory;
-
   constexpr size_t kArgumentCount = 6;
   ArgumentInformation* arguments = new ArgumentInformation[kArgumentCount];
   arguments[0] = {"-i", "--input=", "Path to input file",
                   ArgumentType::kCompositeArgument, true,
-                  is_valid_filename_pointer, is_regular_file_pointer};
+                  &IsValidFilename, &IsRegularFile};
   arguments[1] = {"-o", "--output=", "Path to output directory",
                   ArgumentType::kCompositeArgument, true,
-                  is_valid_filename_pointer, is_directory_pointer};
+                  &IsValidFilename, &IsDirectory};
   arguments[2] = {"-m", "--max-iter=", "Maximum number of iterations",
                   ArgumentType::kLongArgument, false,
-                  is_valid_filename_pointer, is_regular_file_pointer};
+                  &IsValidFilename, &IsRegularFile};
   arguments[3] = {"-f", "--freq=", "Frequency of output",
                   ArgumentType::kLongArgument, false,
-                  is_valid_filename_pointer, is_regular_file_pointer};
+                  &IsValidFilename, &IsRegularFile};
   arguments[4] = {"-w", "--write-tsv", "Write output in TSV format",
                   ArgumentType::kBoolArgument, false,
-                  is_valid_filename_pointer, is_regular_file_pointer};
+                  &IsValidFilename, &IsRegularFile};
   arguments[5] = {"-h", "--help", "Provide help",
                   ArgumentType::kBoolArgument, false,
-                  is_valid_filename_pointer, is_regular_file_pointer};
+                  &IsValidFilename, &IsRegularFile};
 
   ArgumentsParser arguments_parser = ArgumentsParser(arguments, kArgumentCount);
   int8_t exit_code = arguments_parser.ParseArguments(argv, argc);
